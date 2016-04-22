3fd1c5721be30164a33e1c28e6a027f01b0f59d1
lwenke01 <lwenke@gmail.com>
2016-04-21 23:02:41 -0700

    set up basic models

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -2,7 +2,7 @@
 
 module.exports = (mongoose, models)=>{
   let Schema = mongoose.Schema;
-  let UsersSchema = new Schema({
+  let UserSchema = new Schema({
     name: String,
     authentication: {
       email:{
@@ -26,19 +26,38 @@ module.exports = (mongoose, models)=>{
       type: String,
       required: true;
     },
-    userDetails: String,
+    rideStartDate: {
+      type: Date,
+      required: true;
+    },
+    userBio:  String,
+    userCar: {
+      type: String,
+
+    }
     userReviews: [{
       type: Schema.Types.ObjectId,
       ref: 'Review'
     }
-  ]
+  ],
+  userUpdated: {type: Date, default: dateFormat("dddd, mmmm dS, yyyy, h:MM:ss TT")},
 
-    highScore: Number,
-    players: Number
-    updated: {type: Date, default: Date.now},
 
   });
 
-  let Users = mongoose.model('User', UsersSchema);
+
+userSchema.methods.hashPassword = function(password){
+  var hash = this.authentication.password = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+  return hash;
+};
+
+userSchema.methods.comparePassword = function(password){
+  return bcrypt.compareSync(password, this.authentication.password);
+};
+userSchema.methods.generateToken = function(){
+  return jwt.sign({id: this._id}, config.secret);
+};
+
+  let User = mongoose.model('User', UserSchema);
   models.User = User;
 };
